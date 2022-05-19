558356970307bd3aba2015e4899d536be72d0777
Gareth Latty <gareth@lattyware.co.uk>
2022-05-19 20:50:41 +0100

    Add modes to speed up adding new users, new people.

--- update/src/update.mjs
+++ update/src/update.mjs
@@ -1,7 +1,8 @@
 import * as Discord from "discord.js";
+import { open } from "fs";
 import * as Fs from "fs/promises";
-
-const filename = "../data/jads-results.json";
+import { default as Yargs } from "yargs";
+import { hideBin } from "yargs/helpers";
 
 const dichotomies = JSON.parse(
   await Fs.readFile("../data/dichotomies.json", "utf8"),
@@ -27,21 +28,23 @@ function canonicalize(result) {
   if (isCanonical.test(result)) {
     return result;
   } else {
-    return result
+    const canonicalResult = result
       .toLowerCase()
       .replaceAll(
         new RegExp(isEmoji.source, "gu"),
         (match) => canonical.get(match) ?? "",
       );
+    if (isCanonical.test(canonicalResult)) {
+      return canonicalResult;
+    } else {
+      throw new Error(
+        `Could not canonicalize "${result} (got ${canonicalResult}).`,
+      );
+    }
   }
 }
 
-const file = JSON.parse(await Fs.readFile(filename, "utf8"));
-
-const client = new Discord.Client({ intents: 0 });
-await client.login(process.env.DISCORD_TOKEN);
-
-async function updateUser(old) {
+async function updateUser(client, old) {
   try {
     const { username, discriminator, avatar } = await client.users.fetch(
       old.userId,
@@ -59,10 +62,76 @@ async function updateUser(old) {
   }
 }
 
-await Fs.writeFile(
-  filename,
-  `${JSON.stringify(await Promise.all(file.map(updateUser)), undefined, 2)}\n`,
-  { encoding: "utf8" },
-);
+const isAddition = (user) => !Object.hasOwn(user, "name");
 
-await client.destroy();
+async function main(
+  token = process.env.DISCORD_TOKEN,
+  inputFile = "../data/jads-results.json",
+  outputFile = "../data/jads-results.json",
+  addOnly = false,
+) {
+  const inputData = await Fs.readFile(
+    inputFile !== "--" ? inputFile : "/dev/stdin",
+    "utf8",
+  );
+  const results = JSON.parse(inputData);
+
+  const byUser = new Map();
+  for (const result of results) {
+    const existing = byUser.get(result.userId);
+    if (existing) {
+      console.error(existing);
+      console.error(result);
+      throw new Error("Duplicate user ids.");
+    }
+  }
+
+  const client = new Discord.Client({ intents: 0 });
+  await client.login(token);
+
+  const update = addOnly
+    ? (old) => (isAddition(old) ? updateUser(client, old) : old)
+    : (old) => updateUser(client, old);
+  const updatedResults = await Promise.all(results.map(update));
+
+  await client.destroy();
+
+  const outputData = `${JSON.stringify(updatedResults, undefined, 2)}\n`;
+  if (outputFile !== "--") {
+    await Fs.writeFile(outputFile, outputData, { encoding: "utf8" });
+  } else {
+    process.stdout.write(outputData, "utf8");
+  }
+}
+
+const args = Yargs(hideBin(process.argv))
+  .option("add-only", {
+    alias: "a",
+    type: "boolean",
+    description: "Only add new users",
+    default: false,
+  })
+  .option("input-file", {
+    alias: "f",
+    type: "string",
+    describe: "The file to read from.",
+    default: "../data/jads-results.json",
+  })
+  .option("output-file", {
+    alias: "o",
+    type: "string",
+    describe: "The file to write to.",
+    default: "../data/jads-results.json",
+  })
+  .option("token", {
+    alias: "t",
+    type: "string",
+    describe: "The Discord token to use",
+    default: process.env.DISCORD_TOKEN,
+  })
+  .help("help")
+  .alias("h", "help")
+  .version(false)
+  .parse();
+
+await main(args.token, args.inputFile, args.outputFile, args.addOnly);

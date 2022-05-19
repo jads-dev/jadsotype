12bba361bb0f6552230c315976c4194498c719c5
Gareth Latty <gareth@lattyware.co.uk>
2022-05-19 05:32:08 +0100

    New results added.

--- update/src/update.mjs
+++ update/src/update.mjs
@@ -3,6 +3,39 @@ import * as Fs from "fs/promises";
 
 const filename = "../data/jads-results.json";
 
+const dichotomies = JSON.parse(
+  await Fs.readFile("../data/dichotomies.json", "utf8"),
+);
+
+const canonical = new Map(
+  dichotomies.flatMap(({ ends }) => ends).map((end) => [end.emoji, end.id]),
+);
+const isEmoji = new RegExp(
+  `${dichotomies
+    .flatMap(({ ends }) => ends)
+    .map((end) => end.emoji)
+    .join("|")}|\\s`,
+  "u",
+);
+const isCanonical = new RegExp(
+  `^${dichotomies
+    .flatMap(({ ends }) => `[${ends[0].id}${ends[1].id}]`)
+    .join("")}$`,
+  "u",
+);
+function canonicalize(result) {
+  if (isCanonical.test(result)) {
+    return result;
+  } else {
+    return result
+      .toLowerCase()
+      .replaceAll(
+        new RegExp(isEmoji.source, "gu"),
+        (match) => canonical.get(match) ?? "",
+      );
+  }
+}
+
 const file = JSON.parse(await Fs.readFile(filename, "utf8"));
 
 const client = new Discord.Client({ intents: 0 });
@@ -18,6 +51,7 @@ async function updateUser(old) {
       name: username,
       discriminator,
       avatar: avatar ?? undefined,
+      result: canonicalize(old.result),
     };
   } catch (e) {
     console.error(e);
